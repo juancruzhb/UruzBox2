b3d781ec9c0656574fc9addf9c1a0957be7705d3
Juan Cruz <bhjuancruz@gmail.com>
2023-11-09 20:18:06 -0300

    Se crean capas y algunas clases principales

---
+++ UruzBox/queryBase.sql
@@ -0,0 +1,16 @@
+use UruzDB
+
+create table Roles (
+	Id int not null primary key identity(1,1),
+	Nombre varchar (50) not null
+)
+GO
+
+create table Usuarios (
+	Id int not null primary key identity (1,1),
+	Nombre Varchar(50) Not null,
+	Apellido varchar(50) Not null,
+	Usuario varchar (20) not null,
+	Contraseña varchar(10) not null,
+	Role int not null FOREIGN KEY REFERENCES Roles(Id),
+	Activo bit not null)
